eef21dd4607d19a200c645d6c84d042c7fde9a0f
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-05-26 11:39:59 -0300

    Update and rename supabase.js to none

--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'tu-supabase-url';
-const supabaseKey = 'tu-anon-key';
-export const supabase = createClient(supabaseUrl, supabaseKey);
