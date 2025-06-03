7c904b68d0c45297369b0cfd0f41718b8399fd9e
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-06-03 16:51:48 -0300

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ module.exports = function override(config) {
     ...config.resolve.fallback,
     path: require.resolve('path'),
     stream: require.resolve('stream'),
+    assert: require.resolve('assert'),
   };
   return config;
 };
