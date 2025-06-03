aac48f3aa8713e33ad7b52d65de2333247aed564
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-06-03 17:44:22 -0300

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
@@ -6,6 +8,14 @@ module.exports = function override(config) {
     assert: require.resolve('assert'),
     zlib: require.resolve('browserify-zlib'),
     buffer: require.resolve('buffer'),
+    process: require.resolve('process'),
   };
+  config.plugins = [
+    ...(config.plugins || []),
+    new webpack.ProvidePlugin({
+      process: 'process',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ];
   return config;
 };
