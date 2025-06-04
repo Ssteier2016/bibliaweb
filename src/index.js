45ce6345af932463053a5f25d2d2ba98cf5bb24d
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-06-04 15:59:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { register } from './serviceWorkerRegistration'; // Importar el registro
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+// Registrar el Service Worker
+register();
+
+reportWebVitals();
